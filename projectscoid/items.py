71012aed7129bca0c26e837b4b182d8738fefaa6
Armanda Arif <ceedadev@gmail.com>
2021-11-04 17:00:18 +0700

    implement item

--- projectscoid/items.py
+++ projectscoid/items.py
@@ -3,10 +3,30 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy.item import Item, Field
+from datetime import datetime as dt
 
+def dateTimeSerializer (value:str):
+    # 04/11/2021 16:15:58 WIB
+    dateTime = dt.strptime(date_string=value, format="%d/%m/%Y %H:%M:%S WIB")
+    return dateTime
 
-class ProjectscoidItem(scrapy.Item):
+class ProjectsItem(Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = Field()
+    description = Field()
+    tags = Field()
+    publishedBudget = Field()
+    publishedDate = Field(serializer=dateTimeSerializer)
+    selectDeadline = Field(serializer=dateTimeSerializer)
+    finishDays = Field()
+    projectStatus = Field()
+    bidCount = Field()
+    needWeeklyReport = Field()
+    projectOwnerUser = Field()
+    projectOwnerLocation = Field()
+    projectOwnerStars = Field()
+    projectOwnerScore = Field()
+    projectOwnerNoProjects = Field()
+    projectOwnerPoint = Field()
+    projectOwnerRanking = Field()
